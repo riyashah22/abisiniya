6288917c6e43642abde03faa501da94cd62376bc
vansh132 <shah.vansh132@gmail.com>
2024-05-10 23:21:02 +0530

    bottom navigation bar: warnings removed

--- lib/bottom_navigation.dart
+++ lib/bottom_navigation.dart
@@ -15,8 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
   static const List<Widget> _widgetOptions = <Widget>[
     Apartments(),
     VehicleScreen(),
