ed5ff2218bd5ab964af326f9da634e05eba7158f
Riya Shah <riyashah@192.168.1.2>
2024-05-13 21:43:42 +0530

    made it structured

---
+++ lib/models/apartment.dart
@@ -0,0 +1,21 @@
+class Apartment {
+  final String image;
+  final String text;
+  final String address;
+  final String location;
+  final int guest;
+  final int bathroom;
+  final int bedroom;
+  final int price;
+
+  Apartment({
+    required this.image,
+    required this.text,
+    required this.address,
+    required this.location,
+    required this.guest,
+    required this.bathroom,
+    required this.bedroom,
+    required this.price,
+  });
+}
