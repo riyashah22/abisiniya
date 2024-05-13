956ffa5cd41ecf99e72c5c15e4aceb1a6275e64a
Riya Shah <riyashah@192.168.1.2>
2024-05-13 20:52:32 +0530

    Warnings removed

--- lib/screens/auth/signup.dart
+++ lib/screens/auth/signup.dart
@@ -1,4 +1,3 @@
-import 'package:abisiniya/screens/auth/otpVerification.dart';
 import 'package:flutter/material.dart';
 import 'package:abisiniya/screens/auth/login.dart';
 import 'package:abisiniya/services/auth_services.dart';
