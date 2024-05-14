85d8d86b7f6b6938f42ad0ebb56018cf9424da7f
Riya Shah <riyashah@192.168.1.2>
2024-05-14 12:27:00 +0530

    Basic Structure added

--- lib/screens/about/about.dart
+++ lib/screens/about/about.dart
@@ -5,8 +5,69 @@ class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("About Screen"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Image.asset("assets/logo.png"),
+          Column(
+            children: [
+              Card(
+                child: Expanded(
+                  child: Container(
+                    // height: 80,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.add_moderator_outlined,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text("About Us"),
+                          ],
+                        ),
+                        Text("Kriva")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                child: Expanded(
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.contact_support,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text("Contact Us"),
+                          ],
+                        ),
+                        Text("Kriva")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Privacy Policy"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
