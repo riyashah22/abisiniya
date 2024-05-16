2de2c964d7280452ba58937b4bdbfd4b348351ac
vansh132 <shah.vansh132@gmail.com>
2024-05-16 13:03:03 +0530

    vehicle screen: vehicle item done

--- lib/screens/vehicles/vehicle_item.dart
+++ lib/screens/vehicles/vehicle_item.dart
@@ -1,6 +1,5 @@
 import 'package:abisiniya/models/vehicles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class VehicleItem extends StatefulWidget {
   final Vehicle vehicle;
@@ -14,73 +13,104 @@ class _VehicleItemState extends State<VehicleItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(16),
-      // color: Colors.red,
+      margin: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 150,
-            width: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: SizedBox(
+              height: 170,
+              width: 180,
+              child: Image.network(
+                widget.vehicle.images,
                 fit: BoxFit.cover,
-                image: NetworkImage(widget.vehicle.images),
               ),
             ),
           ),
-          SizedBox(
-            width: 24,
-          ),
+          const SizedBox(width: 16),
           Expanded(
             child: Container(
-              height: 150,
+              height: 170,
+              padding: const EdgeInsets.symmetric(vertical: 12),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
                     widget.vehicle.model,
-                    style: Theme.of(context).textTheme.headlineLarge,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                    overflow: TextOverflow.fade,
                   ),
-                  Divider(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(widget.vehicle.make),
-                      Text(widget.vehicle.year.toString())
-                    ],
+                  const SizedBox(
+                    height: 4,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
+                  Text(
+                    widget.vehicle.make,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          color: Colors.grey[600],
+                        ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          // Icon(FontAwesomeIcons.gear),
-                          SizedBox(width: 5),
-                          Text(widget.vehicle.transmission),
+                          Expanded(
+                            child: Text(
+                              '${widget.vehicle.price}/day',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    10), // Decrease the border radius
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12,
+                                  vertical: 8), // Adjust padding
+                            ),
+                            child: const Text(
+                              "Book ride",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14, // Adjust font size
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          )
                         ],
                       ),
-                      Row(
-                        children: [
-                          Icon(Icons.gas_meter_outlined),
-                          SizedBox(width: 5),
-                          Text(widget.vehicle.fuelType),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(widget.vehicle.price.toString()),
-                      ElevatedButton(onPressed: () {}, child: Text("Book ride"))
-                    ],
+                    ),
                   ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
--- lib/screens/vehicles/vehicles.dart
+++ lib/screens/vehicles/vehicles.dart
@@ -2,9 +2,10 @@ import 'package:abisiniya/models/vehicles.dart';
 import 'package:abisiniya/screens/vehicles/vehicle_item.dart';
 import 'package:abisiniya/services/vehicle_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class VehicleScreen extends StatefulWidget {
-  const VehicleScreen({super.key});
+  const VehicleScreen({Key? key}) : super(key: key);
 
   @override
   State<VehicleScreen> createState() => _VehicleScreenState();
@@ -20,7 +21,6 @@ class _VehicleScreenState extends State<VehicleScreen>
   void initState() {
     super.initState();
     fetchVehicles();
-
     _tabController = TabController(length: 2, vsync: this);
   }
 
@@ -33,55 +33,71 @@ class _VehicleScreenState extends State<VehicleScreen>
   Future<void> fetchVehicles() async {
     List<Vehicle> fetchedVehicles =
         await vehicleServices.getAllVehicles(context);
-    vehicleList = fetchedVehicles;
+    setState(() {
+      vehicleList = fetchedVehicles;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            TabBar(
+      body: Column(
+        children: <Widget>[
+          TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: 'Cars'),
+              Tab(text: 'Bus'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
               controller: _tabController,
-              tabs: [
-                Tab(text: 'Cars'),
-                Tab(text: 'Bus'),
+              children: [
+                // Content of the first tab (Cars)
+                _buildVehicleList(),
+                Container(
+                  child: const Center(
+                    child: Text('Bus Tab Content'),
+                  ),
+                ),
               ],
             ),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  // Content of the first tab (Cars)
-                  Container(
-                    child: Column(
-                      children: [
-                        Text("data"),
-                        Container(
-                          height: 550,
-                          child: ListView.builder(
-                            itemCount: vehicleList.length,
-                            itemBuilder: (context, index) => VehicleItem(
-                              vehicle: vehicleList[index],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  // Content of the second tab (Bus)
-                  Container(
-                    child: Center(
-                      child: Text('Bus Tab Content'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _buildVehicleList() {
+    if (vehicleList.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(), // Show loading indicator
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Text(
+              "Find your car",
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                  ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: vehicleList.length,
+              itemBuilder: (context, index) => VehicleItem(
+                vehicle: vehicleList[index],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
   }
 }
