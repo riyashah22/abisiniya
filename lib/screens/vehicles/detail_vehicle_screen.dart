a70f5921995ec1538a24526bbd196e134e562d94
vansh132 <shah.vansh132@gmail.com>
2024-05-17 11:54:28 +0530

    vehicle detail screen: completed

--- lib/screens/vehicles/detail_vehicle_screen.dart
+++ lib/screens/vehicles/detail_vehicle_screen.dart
@@ -1,5 +1,6 @@
 import 'package:abisiniya/models/vehicles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class VehicleDetailScreen extends StatelessWidget {
   static const String routeName = '/vehicle-detail-screen';
@@ -14,7 +15,7 @@ class VehicleDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0, // Remove app bar shadow
+        elevation: 0,
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -38,28 +39,35 @@ class VehicleDetailScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    vehicle.model,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat',
-                      letterSpacing: 0.5, // Add some letter spacing
-                      color: Colors.black87, // Adjust color to your preference
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    vehicle.make,
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors
-                          .grey[600], // Adjusted color for a modern appearance
-                      fontFamily: 'Montserrat', // Using a modern font
-                      letterSpacing: 0.2, // Adding a bit of letter spacing
-                      fontStyle:
-                          FontStyle.italic, // Adding italic style for variation
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.green[50],
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 4,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _buildInfoRow(Icons.directions_car, "Model",
+                                vehicle.model, context),
+                            const SizedBox(height: 16),
+                            _buildInfoRow(
+                                Icons.build, "Make", vehicle.make, context),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(height: 16),
                   const Text(
@@ -70,21 +78,51 @@ class VehicleDetailScreen extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 8),
-                  _buildDetailRow(
-                      Icons.local_gas_station, 'Fuel Type', vehicle.fuelType),
-                  _buildDetailRow(
-                      Icons.engineering, 'Engine Size', vehicle.engineSize),
+                  _buildDetailRow(Icons.local_gas_station, 'Fuel Type',
+                      vehicle.fuelType, context),
+                  _buildDetailRow(Icons.engineering, 'Engine Size',
+                      vehicle.engineSize, context),
                   _buildDetailRow(Icons.settings_input_component,
-                      'Transmission', vehicle.transmission),
-                  _buildDetailRow(Icons.public, 'Country', vehicle.country),
+                      'Transmission', vehicle.transmission, context),
                   _buildDetailRow(
-                      Icons.location_on, 'Address', vehicle.address),
+                      Icons.public, 'Country', vehicle.country, context),
                   _buildDetailRow(
-                      Icons.fitness_center, 'Weight', vehicle.weight),
+                      Icons.location_on, 'Address', vehicle.address, context),
                   _buildDetailRow(
-                      Icons.attach_money, 'Price', '\$${vehicle.price}'),
-                  _buildDetailRow(
-                      Icons.calendar_today, 'Year', vehicle.year.toString()),
+                      Icons.fitness_center, 'Weight', vehicle.weight, context),
+                  _buildDetailRow(Icons.price_change_rounded, 'Price',
+                      '${vehicle.price} Per Day', context),
+                  _buildDetailRow(Icons.calendar_today, 'Year',
+                      vehicle.year.toString(), context),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: ElevatedButton.icon(
+                      icon: Icon(Icons.car_rental),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            Theme.of(context).primaryColor, // Background color
+                        foregroundColor: Theme.of(context)
+                            .scaffoldBackgroundColor, // Text color
+                        padding: EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 24), // Button padding
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(8), // Button border radius
+                        ),
+                      ),
+                      label: Text(
+                        "Book Ride now",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
@@ -94,7 +132,44 @@ class VehicleDetailScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildDetailRow(IconData iconData, String title, String value) {
+  Widget _buildInfoRow(
+      IconData iconData, String title, String value, BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Icon(
+          iconData,
+          color: Theme.of(context).primaryColor,
+          size: 24,
+        ),
+        const SizedBox(width: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            Text(
+              " - $value",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDetailRow(
+      IconData iconData, String title, String value, BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 8),
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
@@ -103,25 +178,23 @@ class VehicleDetailScreen extends StatelessWidget {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            const Color(0xFFA5D6A7)
-                .withOpacity(0.85), // Darker green with opacity
-            const Color(0xFF66BB6A)
-                .withOpacity(0.85) // Darker green with opacity
+            Theme.of(context).primaryColor.withOpacity(0.85),
+            Theme.of(context).primaryColor.withOpacity(0.85),
           ],
         ),
-        borderRadius: BorderRadius.circular(6), // Add rounded corners
+        borderRadius: BorderRadius.circular(6),
         border: Border.all(
-          color: const Color(0xFFBDBDBD), // Add border for a classic look
+          color: const Color(0xFFBDBDBD),
         ),
       ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Icon(
-            iconData, // Use the provided icon data
-            color: Colors.black.withOpacity(0.5), // Adjust color as needed
+            iconData,
+            color: Theme.of(context).secondaryHeaderColor.withOpacity(0.6),
           ),
-          const SizedBox(width: 8), // Add some spacing between icon and title
+          const SizedBox(width: 8),
           Expanded(
             flex: 3,
             child: Text(
@@ -133,15 +206,14 @@ class VehicleDetailScreen extends StatelessWidget {
               ),
             ),
           ),
-          const SizedBox(width: 12), // Add some spacing between title and value
+          const SizedBox(width: 12),
           Expanded(
             flex: 5,
             child: Text(
               value,
               style: const TextStyle(
                 fontSize: 16,
-                color: Colors
-                    .black87, // Darken the text color for better readability
+                color: Colors.black87,
               ),
             ),
           ),
