3a87475713ef4b2f7ef1fda99a0037f404b6ef2e
Riya Shah <riyashah@192.168.1.7>
2024-05-18 23:40:41 +0530

    bottomAppBar and booking details added in vehicle screen

--- lib/screens/vehicles/detail_vehicle_screen.dart
+++ lib/screens/vehicles/detail_vehicle_screen.dart
@@ -1,8 +1,10 @@
 import 'package:abisiniya/models/vehicles.dart';
+import 'package:abisiniya/provider/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-class VehicleDetailScreen extends StatelessWidget {
+class VehicleDetailScreen extends StatefulWidget {
   static const String routeName = '/vehicle-detail-screen';
 
   const VehicleDetailScreen({
@@ -10,8 +12,27 @@ class VehicleDetailScreen extends StatelessWidget {
   });
 
   @override
+  State<VehicleDetailScreen> createState() => _VehicleDetailScreenState();
+}
+
+class _VehicleDetailScreenState extends State<VehicleDetailScreen> {
+  DateTime? fromDate;
+  DateTime? toDate;
+
+  double calculateTotalAmount(int price) {
+    if (fromDate != null && toDate != null) {
+      final duration = toDate!.difference(fromDate!);
+      final nights = duration.inDays;
+      return price * nights.toDouble();
+    } else {
+      return 0.0;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final vehicle = ModalRoute.of(context)!.settings.arguments as Vehicle;
+    final user = Provider.of<UserProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
@@ -95,38 +116,237 @@ class VehicleDetailScreen extends StatelessWidget {
                   _buildDetailRow(Icons.calendar_today, 'Year',
                       vehicle.year.toString(), context),
                   const SizedBox(
-                    height: 16,
+                    height: 8,
                   ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.car_rental),
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            Theme.of(context).primaryColor, // Background color
-                        foregroundColor: Theme.of(context)
-                            .scaffoldBackgroundColor, // Text color
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 24), // Button padding
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(8), // Button border radius
-                        ),
-                      ),
-                      label: const Text(
-                        "Book Ride now",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  )
                 ],
               ),
             ),
+
+            // Booking Details Form
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Booking Details',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'From Date:',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  final selectedDate = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime.now(),
+                                    lastDate: DateTime.now()
+                                        .add(const Duration(days: 365)),
+                                  );
+                                  if (selectedDate != null) {
+                                    setState(() {
+                                      fromDate = selectedDate;
+                                      toDate = null; // Reset toDate
+                                    });
+                                  }
+                                },
+                                child: Text(
+                                  fromDate != null
+                                      ? fromDate!.toString().split(' ')[0]
+                                      : 'Select Date',
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'To Date:',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  if (fromDate == null) {
+                                    // Check if 'from' date is selected first
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title:
+                                              const Text("Select 'From' Date"),
+                                          content: const Text(
+                                              "Please select 'from' date first."),
+                                          actions: <Widget>[
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: const Text("OK"),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  } else {
+                                    final selectedDate = await showDatePicker(
+                                      context: context,
+                                      initialDate: fromDate!,
+                                      firstDate: fromDate!,
+                                      lastDate: DateTime.now()
+                                          .add(const Duration(days: 365)),
+                                    );
+                                    if (selectedDate != null) {
+                                      setState(() {
+                                        toDate = selectedDate;
+                                      });
+                                    }
+                                  }
+                                },
+                                child: Text(
+                                  toDate != null
+                                      ? toDate!.toString().split(' ')[0]
+                                      : 'Select Date',
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: Text(
+                        'Total Amount: \$${calculateTotalAmount(vehicle.price)}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 90,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '\$${vehicle.price} / night',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  double totalAmount = calculateTotalAmount(vehicle.price);
+                  if (totalAmount <= 0) {
+                    // If totalAmount is non-positive, either dates are not selected or invalid
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Select Dates"),
+                          content: Text(totalAmount == 0
+                              ? "Please select both 'from' and 'to' dates."
+                              : "Please select a valid date range."),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text("OK"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    // Proceed with booking
+                    if (user.user.token.isEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Alert"),
+                            content: const Text("Please log in first."),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      // Show success dialog and reset dates
+                      setState(() {
+                        fromDate = null;
+                        toDate = null;
+                      });
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Success"),
+                            content: const Text("Booked Successfully"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  }
+                },
+                child: const Text(
+                  'Book Now',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
