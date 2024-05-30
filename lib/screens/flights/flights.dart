d2c4652a3a5aec7eee45b30ff0d2a7c28f31ba30
Riya Shah <riyashah@192.168.1.2>
2024-05-30 19:49:11 +0530

    Flights edited

--- lib/screens/flights/flights.dart
+++ lib/screens/flights/flights.dart
@@ -132,127 +132,129 @@ class _FlightScreenState extends State<FlightScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Flight Request',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 20),
-                DropdownButtonFormField<String>(
-                  value: _from,
-                  decoration: const InputDecoration(labelText: 'From'),
-                  items: _locations.map((String location) {
-                    return DropdownMenuItem<String>(
-                      value: location,
-                      child: Text(location),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      _from = newValue!;
-                    });
-                  },
-                ),
-                DropdownButtonFormField<String>(
-                  value: _to,
-                  decoration: const InputDecoration(labelText: 'To'),
-                  items: _locations.map((String location) {
-                    return DropdownMenuItem<String>(
-                      value: location,
-                      child: Text(location),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      _to = newValue!;
-                    });
-                  },
-                ),
-                DropdownButtonFormField<String>(
-                  value: _airline,
-                  decoration:
-                      const InputDecoration(labelText: 'Airline Preferences'),
-                  items: airlines.map((String airline) {
-                    return DropdownMenuItem<String>(
-                      value: airline,
-                      child: Text(airline),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      _airline = newValue!;
-                    });
-                  },
-                ),
-                DropdownButtonFormField<String>(
-                  value: _class,
-                  decoration: const InputDecoration(labelText: 'Travel Class'),
-                  items: _classes.map((String travelClass) {
-                    return DropdownMenuItem<String>(
-                      value: travelClass,
-                      child: Text(travelClass),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      _class = newValue!;
-                    });
-                  },
-                ),
-                DropdownButtonFormField<String>(
-                  value: _tripType,
-                  decoration: const InputDecoration(labelText: 'Trip Type'),
-                  items: _tripTypes.map((String tripType) {
-                    return DropdownMenuItem<String>(
-                      value: tripType,
-                      child: Text(tripType),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    setState(() {
-                      _tripType = newValue!;
-                    });
-                  },
-                ),
-                ListTile(
-                  title: Text(
-                      'Departure Date: ${_departureDate?.toLocal().toString().split(' ')[0] ?? 'Select Date'}'),
-                  trailing: const Icon(Icons.calendar_today),
-                  onTap: () => _selectDate(context, true),
-                ),
-                ListTile(
-                  title: Text(
-                      'Return Date: ${_returnDate?.toLocal().toString().split(' ')[0] ?? 'Select Date'}'),
-                  trailing: const Icon(Icons.calendar_today),
-                  onTap: () => _selectDate(context, false),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Message'),
-                  maxLines: 3,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _message = newValue;
-                    });
-                  },
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
+        body: SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        width: double.infinity,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Flight Request',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                initialValue: _from, // Set initial value
+                decoration: const InputDecoration(labelText: 'From'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a departure location';
+                  }
+                  return null;
+                },
+                onChanged: (newValue) {
+                  setState(() {
+                    _from = newValue;
+                  });
+                },
+              ),
+              TextFormField(
+                initialValue: _to, // Set initial value
+                decoration: const InputDecoration(labelText: 'To'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a destination location';
+                  }
+                  return null;
+                },
+                onChanged: (newValue) {
+                  setState(() {
+                    _to = newValue;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _airline,
+                decoration:
+                    const InputDecoration(labelText: 'Airline Preferences'),
+                items: airlines.map((String airline) {
+                  return DropdownMenuItem<String>(
+                    value: airline,
+                    child: Text(airline),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _airline = newValue!;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _class,
+                decoration: const InputDecoration(labelText: 'Travel Class'),
+                items: _classes.map((String travelClass) {
+                  return DropdownMenuItem<String>(
+                    value: travelClass,
+                    child: Text(travelClass),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _class = newValue!;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                value: _tripType,
+                decoration: const InputDecoration(labelText: 'Trip Type'),
+                items: _tripTypes.map((String tripType) {
+                  return DropdownMenuItem<String>(
+                    value: tripType,
+                    child: Text(tripType),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _tripType = newValue!;
+                  });
+                },
+              ),
+              ListTile(
+                title: Text(
+                    'Departure Date: ${_departureDate?.toLocal().toString().split(' ')[0] ?? 'Select Date'}'),
+                trailing: const Icon(Icons.calendar_today),
+                onTap: () => _selectDate(context, true),
+              ),
+              ListTile(
+                title: Text(
+                    'Return Date: ${_returnDate?.toLocal().toString().split(' ')[0] ?? 'Select Date'}'),
+                trailing: const Icon(Icons.calendar_today),
+                onTap: () => _selectDate(context, false),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Message'),
+                maxLines: 3,
+                onChanged: (newValue) {
+                  setState(() {
+                    _message = newValue;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
                   onPressed: _submitForm,
                   child: const Text('Submit'),
                 ),
-                const SizedBox(height: 10),
-              ],
-            ),
+              ),
+              const SizedBox(height: 10),
+            ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
