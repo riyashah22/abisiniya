bbdac5d75d67652d901653ff5d22530e6daed95d
Riya Shah <riyashah@192.168.1.2>
2024-05-31 16:08:05 +0530

    flights working

--- lib/screens/flights/flights.dart
+++ lib/screens/flights/flights.dart
@@ -1,3 +1,4 @@
+import 'package:abisiniya/constants/error_handling.dart';
 import 'package:abisiniya/provider/user.dart';
 import 'package:abisiniya/screens/auth/login.dart';
 import 'package:abisiniya/services/flight_services.dart';
@@ -5,97 +6,30 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class FlightScreen extends StatefulWidget {
-  const FlightScreen({Key? key}) : super(key: key);
+  const FlightScreen({super.key});
 
   @override
-  _FlightScreenState createState() => _FlightScreenState();
+  State<FlightScreen> createState() => _FlightScreenState();
 }
 
 class _FlightScreenState extends State<FlightScreen> {
   final _formKey = GlobalKey<FormState>();
-  bool isLoggedIn = false;
+  final FlightServices flightServices = FlightServices();
 
-  String _from = 'New York';
-  String _to = 'Los Angeles';
-
+  String _from = '';
+  String _to = '';
+  String? _airline;
   String _class = 'Economy';
   String _tripType = 'One Way';
-  DateTime? _departureDate;
-  DateTime? _returnDate;
+  DateTime? fromDate;
+  DateTime? toDate;
   String _message = '';
 
-  FlightServices flightServices = FlightServices();
-  List<String> airlines = [];
-  String? _airline;
-
-  @override
-  void initState() {
-    super.initState();
-    getAirlines();
-  }
-
-  Future<void> getAirlines() async {
-    List<String> fetchAirlines = await flightServices.airlines(context);
-    setState(() {
-      airlines = fetchAirlines;
-    });
-  }
-
-  final List<String> _locations = [
-    'New York',
-    'Los Angeles',
-    'Chicago',
-    'Houston'
-  ];
+  List<String> _airports = [];
+  List<String> _airlines = [];
 
   final List<String> _classes = ['Economy', 'Business', 'First Class'];
   final List<String> _tripTypes = ['One Way', 'Round Trip', 'Multi-City'];
-
-  void _showLoginDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Login Required'),
-        content: const Text('Please log in first.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
-            },
-            child: const Text('Login'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _selectDate(BuildContext context, bool isDeparture) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2020),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null) {
-      setState(() {
-        if (isDeparture) {
-          _departureDate = picked;
-          if (_returnDate != null && _departureDate!.isAfter(_returnDate!)) {
-            _showValidationDialog('Departure date cannot be after return date');
-            _returnDate = null; // Clear return date if it conflicts
-          }
-        } else {
-          if (_departureDate != null && picked.isBefore(_departureDate!)) {
-            _showValidationDialog(
-                'Return date cannot be before departure date');
-          } else {
-            _returnDate = picked;
-          }
-        }
-      });
-    }
-  }
 
   void _showValidationDialog(String message) {
     showDialog(
@@ -113,148 +47,319 @@ class _FlightScreenState extends State<FlightScreen> {
     );
   }
 
-  void _submitForm() {
-    if (_formKey.currentState?.validate() ?? false) {
-      if (_tripType == 'Round Trip' &&
-          (_departureDate == null || _returnDate == null)) {
-        _showValidationDialog(
-            'Both departure and return dates are required for round trips');
-        return;
-      }
-
-      // Handle form submission
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Form Submitted')),
+  void _submitForm() async {
+    final user = Provider.of<UserProvider>(context, listen: false);
+    if (user.user.token.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Alert"),
+            content: const Text("Please log in first."),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushReplacementNamed(LoginScreen.routeName);
+                },
+                child: const Text("Login"),
+              ),
+            ],
+          );
+        },
       );
+      showSnackBar(context, "Please login");
+    } else {
+      if (_formKey.currentState?.validate() ?? false) {
+        if (_tripType == 'Round Trip' && (fromDate == null || toDate == null)) {
+          _showValidationDialog(
+              'Both departure and return dates are required for round trips');
+          return;
+        }
+
+        await flightServices.flightRequest(
+            context,
+            _from,
+            _to,
+            fromDate.toString(),
+            toDate?.toString() ?? '',
+            _airline!,
+            _class,
+            _tripType,
+            _message);
+      }
     }
+  }
+
+  Future<void> _getAirports() async {
+    _airports = await flightServices.airports(context);
+    setState(() {});
+  }
+
+  Future<void> _getAirlines() async {
+    _airlines = await flightServices.airlines(context);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _getAirports();
+    _getAirlines();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        width: double.infinity,
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Flight Request',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                initialValue: _from, // Set initial value
-                decoration: const InputDecoration(labelText: 'From'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a departure location';
-                  }
-                  return null;
-                },
-                onChanged: (newValue) {
-                  setState(() {
-                    _from = newValue;
-                  });
-                },
-              ),
-              TextFormField(
-                initialValue: _to, // Set initial value
-                decoration: const InputDecoration(labelText: 'To'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a destination location';
-                  }
-                  return null;
-                },
-                onChanged: (newValue) {
-                  setState(() {
-                    _to = newValue;
-                  });
-                },
-              ),
-              DropdownButtonFormField<String>(
-                value: _airline,
-                decoration:
-                    const InputDecoration(labelText: 'Airline Preferences'),
-                items: airlines.map((String airline) {
-                  return DropdownMenuItem<String>(
-                    value: airline,
-                    child: Text(airline),
-                  );
-                }).toList(),
-                onChanged: (newValue) {
-                  setState(() {
-                    _airline = newValue!;
-                  });
-                },
-              ),
-              DropdownButtonFormField<String>(
-                value: _class,
-                decoration: const InputDecoration(labelText: 'Travel Class'),
-                items: _classes.map((String travelClass) {
-                  return DropdownMenuItem<String>(
-                    value: travelClass,
-                    child: Text(travelClass),
-                  );
-                }).toList(),
-                onChanged: (newValue) {
-                  setState(() {
-                    _class = newValue!;
-                  });
-                },
-              ),
-              DropdownButtonFormField<String>(
-                value: _tripType,
-                decoration: const InputDecoration(labelText: 'Trip Type'),
-                items: _tripTypes.map((String tripType) {
-                  return DropdownMenuItem<String>(
-                    value: tripType,
-                    child: Text(tripType),
-                  );
-                }).toList(),
-                onChanged: (newValue) {
-                  setState(() {
-                    _tripType = newValue!;
-                  });
-                },
-              ),
-              ListTile(
-                title: Text(
-                    'Departure Date: ${_departureDate?.toLocal().toString().split(' ')[0] ?? 'Select Date'}'),
-                trailing: const Icon(Icons.calendar_today),
-                onTap: () => _selectDate(context, true),
-              ),
-              ListTile(
-                title: Text(
-                    'Return Date: ${_returnDate?.toLocal().toString().split(' ')[0] ?? 'Select Date'}'),
-                trailing: const Icon(Icons.calendar_today),
-                onTap: () => _selectDate(context, false),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Message'),
-                maxLines: 3,
-                onChanged: (newValue) {
-                  setState(() {
-                    _message = newValue;
-                  });
-                },
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text('Submit'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Flight Request',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 10),
-            ],
+                const SizedBox(height: 20),
+                _buildAutocomplete(
+                  labelText: 'From',
+                  options: _airports,
+                  onSelected: (selection) {
+                    setState(() {
+                      _from = selection;
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                _buildAutocomplete(
+                  labelText: 'To',
+                  options: _airports,
+                  onSelected: (selection) {
+                    setState(() {
+                      _to = selection;
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                _buildAutocomplete(
+                  labelText: 'Airline Preferences',
+                  options: _airlines,
+                  onSelected: (selection) {
+                    setState(() {
+                      _airline = selection;
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                _buildDropdown(
+                  labelText: 'Travel Class',
+                  value: _class,
+                  items: _classes,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _class = newValue!;
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                _buildDropdown(
+                  labelText: 'Trip Type',
+                  value: _tripType,
+                  items: _tripTypes,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _tripType = newValue!;
+                      if (_tripType == 'One Way') {
+                        toDate = null;
+                      }
+                    });
+                  },
+                ),
+                const SizedBox(height: 10),
+                _buildDateSelection(
+                  enabled: true,
+                  context,
+                  label: 'From Date:',
+                  date: fromDate,
+                  onSelectDate: () async {
+                    final selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now().add(const Duration(days: 365)),
+                    );
+                    if (selectedDate != null) {
+                      setState(() {
+                        fromDate = selectedDate;
+                        toDate = null; // Reset toDate
+                      });
+                    }
+                  },
+                ),
+                const SizedBox(height: 10),
+                _buildDateSelection(
+                  context,
+                  label: 'To Date:',
+                  date: toDate,
+                  onSelectDate: () async {
+                    if (fromDate == null) {
+                      _showValidationDialog("Please select 'From' date first.");
+                    } else {
+                      final selectedDate = await showDatePicker(
+                        context: context,
+                        initialDate: fromDate!,
+                        firstDate: fromDate!,
+                        lastDate: DateTime.now().add(const Duration(days: 365)),
+                      );
+                      if (selectedDate != null) {
+                        setState(() {
+                          toDate = selectedDate;
+                        });
+                      }
+                    }
+                  },
+                  enabled: _tripType != 'One Way',
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 2.0),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Message',
+                        border: InputBorder.none,
+                      ),
+                      maxLines: 2,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _message = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _submitForm,
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
+  }
+
+  Widget _buildAutocomplete({
+    required String labelText,
+    required List<String> options,
+    required Function(String) onSelected,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Autocomplete<String>(
+          optionsBuilder: (TextEditingValue textEditingValue) {
+            if (textEditingValue.text.isEmpty) {
+              return const Iterable<String>.empty();
+            }
+            return options.where((String option) {
+              return option
+                  .toLowerCase()
+                  .contains(textEditingValue.text.toLowerCase());
+            });
+          },
+          onSelected: (String selection) {
+            onSelected(selection);
+          },
+          fieldViewBuilder: (BuildContext context,
+              TextEditingController textEditingController,
+              FocusNode focusNode,
+              VoidCallback onFieldSubmitted) {
+            return TextFormField(
+              controller: textEditingController,
+              focusNode: focusNode,
+              decoration: InputDecoration(
+                labelText: labelText,
+                border: InputBorder.none,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a $labelText';
+                }
+                return null;
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDropdown({
+    required String labelText,
+    required String value,
+    required List<String> items,
+    required Function(String?) onChanged,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: DropdownButtonFormField<String>(
+          value: value,
+          decoration: InputDecoration(
+            labelText: labelText,
+            border: InputBorder.none,
+          ),
+          items: items.map((String item) {
+            return DropdownMenuItem<String>(
+              value: item,
+              child: Text(item),
+            );
+          }).toList(),
+          onChanged: onChanged,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDateSelection(
+    BuildContext context, {
+    required String label,
+    required DateTime? date,
+    required Function() onSelectDate,
+    required bool enabled, // Added parameter for enabling/disabling the field
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: ListTile(
+        title: Text(
+          '$label ${date != null ? date.toString().split(' ')[0] : 'Select Date'}',
+        ),
+        trailing: const Icon(Icons.calendar_today),
+        onTap: enabled ? onSelectDate : null, // Disable onTap when not enabled
+      ),
+    );
   }
 }
--- lib/services/flight_services.dart
+++ lib/services/flight_services.dart
@@ -1,14 +1,68 @@
 import 'dart:convert'; // Required for jsonDecode
 import 'package:abisiniya/constants/error_handling.dart';
+import 'package:abisiniya/provider/user.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FlightServices {
+  Future<void> flightRequest(
+      BuildContext context,
+      String from,
+      String to,
+      String fromDate,
+      String toDate,
+      String airline,
+      String travel_class,
+      String trip_option,
+      String message) async {
+    final user = Provider.of<UserProvider>(context, listen: false);
+
+    try {
+      final request = {
+        'from': from,
+        'to': to,
+        'airline': airline,
+        'departure_date': fromDate,
+        'return_date': toDate,
+        'travel_class': travel_class,
+        'trip_option': trip_option,
+        'message': message,
+      };
+
+      final uri = Uri.parse(
+        "https://staging.abisiniya.com/api/v1/flight/auth/userflight",
+      );
+
+      final res = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${user.user.token}',
+        },
+        body: jsonEncode(request),
+      );
+
+      httpErrorHandle(
+        response: res,
+        onError: (errorMessage) {
+          showSnackBar(context, errorMessage);
+        },
+        onSuccess: () {
+          showSnackBar(context, "Flight Request Submitted");
+        },
+      );
+    } catch (e) {
+      final errorMessage = "Error occurred: ${e.toString()}";
+      showSnackBar(context, errorMessage);
+    }
+  }
+
   Future<List<String>> airlines(BuildContext context) async {
     try {
       http.Response res = await http.get(
         Uri.parse(
-          "https://staging.abisiniya.com/api/v1/flight/airportlist",
+          "https://staging.abisiniya.com/api/v1/flight/airlinelist",
         ),
       );
 
@@ -23,7 +77,6 @@ class FlightServices {
               airlines.add(airport['name']);
             }
           }
-          print(airlines);
         },
         onError: (errorMessage) {
           showSnackBar(context, errorMessage);
@@ -34,7 +87,40 @@ class FlightServices {
     } catch (e) {
       final errorMessage = "Error occurred: ${e.toString()}";
       showSnackBar(context, errorMessage);
-      return [];
+      return []; // Return an empty list on error
+    }
+  }
+
+  Future<List<String>> airports(BuildContext context) async {
+    try {
+      http.Response res = await http.get(
+        Uri.parse(
+          "https://staging.abisiniya.com/api/v1/flight/airportlist",
+        ),
+      );
+
+      List<String> airports = [];
+
+      httpErrorHandle(
+        response: res,
+        onSuccess: () {
+          final decodedResponse = jsonDecode(res.body);
+          if (decodedResponse['success']) {
+            for (var airport in decodedResponse['data']) {
+              airports.add(airport['name']);
+            }
+          }
+        },
+        onError: (errorMessage) {
+          showSnackBar(context, errorMessage);
+        },
+      );
+
+      return airports;
+    } catch (e) {
+      final errorMessage = "Error occurred: ${e.toString()}";
+      showSnackBar(context, errorMessage);
+      return []; // Return an empty list on error
     }
   }
 }
