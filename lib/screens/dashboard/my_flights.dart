a0c9bb59ea96d1a302e4c98683cd5ec882213f89
Riya Shah <riyashah@192.168.1.5>
2024-06-06 14:21:25 +0530

    My flight request ui changed

--- lib/screens/dashboard/my_flights.dart
+++ lib/screens/dashboard/my_flights.dart
@@ -1,4 +1,7 @@
+import 'package:abisiniya/services/flight_services.dart';
+import 'package:abisiniya/themes/custom_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyFlights extends StatefulWidget {
   const MyFlights({super.key});
@@ -8,8 +11,262 @@ class MyFlights extends StatefulWidget {
 }
 
 class _MyFlightsState extends State<MyFlights> {
+  List<dynamic> myFlights = [];
+  FlightServices flightServices = FlightServices();
+
+  Future<void> fetchUserApartments() async {
+    final flights = await flightServices.userFlightRequests(context);
+    if (flights != null) {
+      setState(() {
+        myFlights = flights;
+      });
+    }
+    print(myFlights[0]['from']);
+  }
+
+  @override
+  void initState() {
+    fetchUserApartments();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: ListView.builder(
+        itemCount: myFlights.length,
+        itemBuilder: (context, index) {
+          return FlightRequestItem(request: myFlights[index]);
+        },
+      ),
+    );
+  }
+
+  Widget FlightRequestItem({required dynamic request}) {
+    return Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            color: Colors.white,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/aeroplane.png",
+                        height: 20,
+                        width: 20,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        request['airline'],
+                        style: GoogleFonts.roboto(
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 12),
+                  Divider(
+                    height: 10,
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Icon(Icons.flight_takeoff_outlined),
+                      SizedBox(width: 4),
+                      Expanded(
+                        child: Text(
+                          request['from'],
+                          softWrap: true,
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      Icon(Icons.flight_land_outlined),
+                      SizedBox(width: 4),
+                      Expanded(
+                        child: Text(
+                          request['to'],
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Departure Date',
+                              style: GoogleFonts.roboto(
+                                color: Color(0xff708090),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(request['departure_date']),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Expanded(
+                          flex: 2,
+                          child: Icon(
+                            Icons.arrow_forward,
+                            size: 35,
+                          )),
+                      SizedBox(width: 8),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Return Date',
+                              style: GoogleFonts.roboto(
+                                color: Color(0xff708090),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(
+                              request['return_date'] == null
+                                  ? 'N/A'
+                                  : request['return_date'].toString(),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 32),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        width:
+                            120, // Adjust width to control the horizontal size
+                        height:
+                            40, // Adjust height to control the vertical size
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              25), // Adjust the radius to make it more oval
+                          border: Border.all(
+                            color: CustomColors
+                                .primaryColor, // Optional border color
+                            width: 2, // Optional border width
+                          ),
+                        ),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.airline_seat_recline_extra_sharp),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              Text(
+                                request['travel_class'],
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width:
+                            120, // Adjust width to control the horizontal size
+                        height:
+                            50, // Adjust height to control the vertical size
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              25), // Adjust the radius to make it more oval
+                          border: Border.all(
+                            color: CustomColors
+                                .primaryColor, // Optional border color
+                            width: 2, // Optional border width
+                          ),
+                        ),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              request['trip_option'] == 'One Way'
+                                  ? Icon(Icons.location_on)
+                                  : Icon(Icons.autorenew),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              Text(
+                                request['trip_option'],
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 32),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Payment Status:",
+                        style: GoogleFonts.roboto(
+                          color: Color(0xff708090),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        request['status'],
+                        style: GoogleFonts.roboto(
+                          color: request['status'] == 'Not Paid'
+                              ? Colors.red
+                              : Colors.green,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
